54de57752aca60957d67bfb7f4c6a8e565ea54ac
shukawam <shukawam@gmail.com>
2022-11-16 21:26:34 +0900

    Fixed

--- opensearch/main-bastion.tf
+++ opensearch/main-bastion.tf
@@ -1,11 +1,18 @@
 resource "oci_core_instance" "bastion_instance" {
-  #Required
-  #   availability_domain = var.instance_availability_domain
-  #   compartment_id      = var.compartment_id
-  #   shape               = var.instance_shape
   availability_domain = var.instance_availability_domain
   compartment_id      = var.compartment_id
   shape               = var.instance_shape
+  shape_config {
+    memory_in_gbs = "6"
+    ocpus         = "1"
+  }
+
+  display_name = var.instance_display_name
+  create_vnic_details {
+    assign_private_dns_record = "false"
+    assign_public_ip          = "true"
+    subnet_id                 = oci_core_subnet.opensearch-subnet.id
+  }
 
   metadata = {
     ssh_authorized_keys = var.ssh_public_key
--- opensearch/variables.tf
+++ opensearch/variables.tf
@@ -1,110 +1,93 @@
+### commons
 variable "compartment_id" {}
 variable "region" {}
 
-variable "instance_availability_domain" {
-  default = "AD-1"
+### compute
+variable "instance_display_name" {
+  default = "opensearch-bastion"
 }
-
+variable "instance_availability_domain" {
+  default = "TGjA:PHX-AD-1"
+}
 variable "instance_shape" {
   default = "VM.Standard.A1.Flex"
 }
-
 variable "ssh_public_key" {
   description = "SSH Public Key"
 }
 
+### network
 variable "security_list_ingress_security_rules_protocol" {
-  default = 6
+  default = 6 # TCP
 }
-
 variable "security_list_ingress_security_rules_source" {
   default = "0.0.0.0/0"
 }
-
 variable "security_list_ingress_security_rules_tcp_options_destination_port_api" {
   default = 9200
 }
-
 variable "security_list_ingress_security_rules_tcp_options_destination_port_dashboard" {
   default = 5601
 }
 
+### OpenSearch Cluster
 variable "opensearch_cluster_data_node_count" {
   default = 1
 }
-
 variable "opensearch_cluster_data_node_host_bare_metal_shape" {
   default = "dataNodeHostBareMetalShape"
 }
-
 variable "opensearch_cluster_data_node_host_memory_gb" {
   default = 20
 }
-
 variable "opensearch_cluster_data_node_host_ocpu_count" {
   default = 1
 }
-
 variable "opensearch_cluster_data_node_host_type" {
   default = "FLEX"
 }
-
 variable "opensearch_cluster_data_node_storage_gb" {
   default = 50
 }
-
 variable "opensearch_cluster_display_name" {
   default = "shukawam-dev-cluster"
 }
-
 variable "opensearch_cluster_freeform_tags" {
   default = { "ManagedByResourceManager" = "true" }
 }
-
 variable "opensearch_cluster_id" {
   default = "id"
 }
-
 variable "opensearch_cluster_master_node_count" {
   default = 1
 }
-
 variable "opensearch_cluster_master_node_host_bare_metal_shape" {
   default = "masterNodeHostBareMetalShape"
 }
-
 variable "opensearch_cluster_master_node_host_memory_gb" {
   default = 20
 }
-
 variable "opensearch_cluster_master_node_host_ocpu_count" {
   default = 1
 }
-
 variable "opensearch_cluster_master_node_host_type" {
   default = "FLEX"
 }
-
 variable "opensearch_cluster_opendashboard_node_count" {
   default = 1
 }
-
 variable "opensearch_cluster_opendashboard_node_host_memory_gb" {
   default = 8
 }
-
 variable "opensearch_cluster_opendashboard_node_host_ocpu_count" {
   default = 1
 }
-
 variable "opensearch_cluster_software_version" {
   default = "1.2.4"
 }
-
 variable "opensearch_cluster_state" {
   default = "ACTIVE"
 }
-
 variable "opensearch_cluster_system_tags" {
   default = {}
 }
