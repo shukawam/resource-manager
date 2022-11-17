7edacfa82c600c56dd5a4082372cee201027fd73
shukawam <shukawam@gmail.com>
2022-11-17 10:40:15 +0900

    Added data source

--- opensearch/main-network.tf
+++ opensearch/main-network.tf
@@ -7,7 +7,7 @@ resource "oci_core_subnet" "opensearch-subnet" {
   cidr_block     = var.subnet_cidr_block
   compartment_id = var.compartment_id
   vcn_id         = oci_core_vcn.opensearch-vcn.id
-  security_list_ids = oci_core_security_list.opensearch-security-list.security_lists
+  security_list_ids = oci_core_security_list.opensearch-security-lists.security_lists
 }
 
 resource "oci_core_security_list" "opensearch-security-list" {
@@ -29,4 +29,9 @@ resource "oci_core_security_list" "opensearch-security-list" {
       min = var.security_list_ingress_security_rules_tcp_options_destination_port_dashboard
     }
   }
+}
+
+data "oci_core_security_lists" "opensearch-security-lists" {
+  compartment_id = var.compartment_id
+  vcn_id         = oci_core_vcn.opensearch-vcn.id
 }
