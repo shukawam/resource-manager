e23a1fb62831acfdf63e9985f28e6feb272f53be
shukawam <shukawam@gmail.com>
2022-11-17 10:54:33 +0900

    Test

--- opensearch/variables.tf
+++ opensearch/variables.tf
@@ -29,6 +29,10 @@ variable "instance_source_type" {
 }
 
 ### network
+data "oci_core_security_lists" "opensearch-security-lists" {
+  compartment_id = var.compartment_id
+  vcn_id         = oci_core_vcn.opensearch-vcn.id
+}
 variable "vcn_cidr_block" {
   default = "10.0.0.0/16"
 }
