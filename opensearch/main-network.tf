241914bd9a2bb97257bbf5486ef8be2b64ab4298
shukawam <shukawam@gmail.com>
2022-11-18 12:01:14 +0900

    Updated

--- opensearch/main-network.tf
+++ opensearch/main-network.tf
@@ -7,7 +7,7 @@ resource "oci_core_subnet" "opensearch-subnet" {
   cidr_block     = var.subnet_cidr_block
   compartment_id = var.compartment_id
   vcn_id         = oci_core_vcn.opensearch-vcn.id
-  # security_list_ids = data.oci_core_security_lists.opensearch-security-lists.security_lists
+  security_list_ids = [oci_core_security_list.opensearch-security-list.id]
 }
 
 resource "oci_core_security_list" "opensearch-security-list" {
@@ -31,16 +31,18 @@ resource "oci_core_security_list" "opensearch-security-list" {
   }
 }
 
-# resource "oci_core_internet_gateway" "opensearch-internet-gateway" {
-#   compartment_id = var.compartment_id
-#   vcn_id         = oci_core_vcn.opensearch-vcn.id
-#   enabled        = var.internet_gateway_enabled
-# }
+resource "oci_core_internet_gateway" "opensearch-internet-gateway" {
+  compartment_id = var.compartment_id
+  vcn_id         = oci_core_vcn.opensearch-vcn.id
+  enabled        = var.internet_gateway_enabled
+}
 
-# resource "oci_core_route_table" "opensearch-route-table" {
-#   compartment_id = var.compartment_id
-#   vcn_id         = oci_core_vcn.opensearch-vcn.id
-#   route_rules {
-#     network_entity_id = oci_core_internet_gateway.opensearch-internet-gateway.id
-#   }
-# }
+resource "oci_core_route_table" "opensearch-route-table" {
+  compartment_id = var.compartment_id
+  vcn_id         = oci_core_vcn.opensearch-vcn.id
+  route_rules {
+    destination       = "0.0.0.0/0"
+    destination_type  = "CIDR_BLOCK"
+    network_entity_id = oci_core_internet_gateway.opensearch-internet-gateway.id
+  }
+}
