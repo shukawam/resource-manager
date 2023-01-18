98ece3d44a0422921e543e681d7e2ebb54b38a57
shukawam <shukawam@gmail.com>
2023-01-18 13:09:15 +0900

    wip

--- examples/oke-cluster-for-tutorials/main-kube-config.tf
+++ examples/oke-cluster-for-tutorials/main-kube-config.tf
@@ -6,16 +6,16 @@ variable "cluster_kube_config_token_version" {
   default = "2.0.0"
 }
 
-data "oci_containerengine_cluster_kube_config" "test_cluster_kube_config" {
+data "oci_containerengine_cluster_kube_config" "tutorial_cluster_kube_config" {
   #Required
-  cluster_id = oci_containerengine_cluster.test_cluster.id
+  cluster_id = oci_containerengine_cluster.tutorial_cluster.id
 
   #Optional
   expiration    = var.cluster_kube_config_expiration
   token_version = var.cluster_kube_config_token_version
 }
 
-resource "local_file" "test_cluster_kube_config_file" {
-  content  = data.oci_containerengine_cluster_kube_config.test_cluster_kube_config.content
-  filename = "${path.module}/test_cluster_kubeconfig"
+resource "local_file" "tutorial_cluster_kube_config_file" {
+  content  = data.oci_containerengine_cluster_kube_config.tutorial_cluster_kube_config.content
+  filename = "${path.module}/tutorial_cluster_kubeconfig"
 }
