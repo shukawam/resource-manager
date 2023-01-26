1890b5291dfe1b414381f4a692da96ad36186b70
shukawam <shukawam@gmail.com>
2023-01-26 13:24:46 +0900

    delete data source from main-kube-config

--- examples/oke-cluster-for-tutorials/main-kube-config.tf
+++ examples/oke-cluster-for-tutorials/main-kube-config.tf
@@ -6,11 +6,6 @@ variable "cluster_kube_config_token_version" {
   default = "2.0.0"
 }
 
-data "oci_containerengine_cluster_kube_config" "tutorial_cluster_kube_config" {
-  cluster_id = oci_containerengine_cluster.tutorial_cluster.id
-  expiration    = var.cluster_kube_config_expiration
-  token_version = var.cluster_kube_config_token_version
-}
 resource "local_file" "tutorial_cluster_kube_config_file" {
   content  = data.oci_containerengine_cluster_kube_config.tutorial_cluster_kube_config.content
   filename = "${path.module}/tutorial_cluster_kubeconfig"
